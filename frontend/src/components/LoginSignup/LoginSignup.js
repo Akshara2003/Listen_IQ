3baa6ac639a38d4ba341e427c891f863a7720503
Devika-0-0-3 <mailtodevika2003@gmail.com>
2025-06-30 00:50:12 +0530

    login -sample

--- frontend/src/components/LoginSignup/LoginSignup.js
+++ frontend/src/components/LoginSignup/LoginSignup.js
@@ -1,36 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './LoginSignup.css';
 
 import email_icon from '../assets/email.png';
 import password_icon from '../assets/password.png';
 
 const Login = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
-    <div className='container'>
+    <div className='page-container'>
 
-      <div className='header'>
-        <div className='text'>Log In</div>
-        <div className='underline'></div>
+      <div className='navbar'>
+        Listen IQ
       </div>
 
-      <div className='inputs'>
-        <div className='input'>
-          <img src={email_icon} alt='' />
-          <input type='email' placeholder='Email Id' />
+      <div className='container'>
+
+        <div className='header'>
+          <div className='text'>Log In</div>
+          <div className='underline'></div>
         </div>
 
-        <div className='input'>
-          <img src={password_icon} alt='' />
-          <input type='password' placeholder='Password' />
+        <div className='inputs'>
+          <div className='input'>
+            <img src={email_icon} alt='' />
+            <input type='email' placeholder='email' />
+          </div>
+
+          <div className='input'>
+            <img src={password_icon} alt='' />
+            <input 
+              type={showPassword ? 'text' : 'password'} 
+              placeholder='password' 
+            />
+            <button 
+              type='button' 
+              className='show-btn' 
+              onClick={() => setShowPassword(!showPassword)}
+            >
+              {showPassword ? 'Hide' : 'Show'}
+            </button>
+          </div>
         </div>
+
+        <div className='submit-container'>
+          <div className='submit'>Log In</div>
+        </div>
+
       </div>
-
-      
-
-      <div className='submit-container'>
-        <div className='submit'>Log In</div>
-      </div>
-
     </div>
   );
 }
